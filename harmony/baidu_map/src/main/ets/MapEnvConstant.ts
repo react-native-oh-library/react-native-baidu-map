3428759a316e51b133c494a2bf3a0f08665a2fc7
li2qi <122847078+li2qi@users.noreply.github.com>
2025-04-02 16:45:14 +0800

    fix: 修复地图多次进入地图会白屏问题 (#44)
    Signed-off-by: 李琦 <liqi219@h-partners.com>

--- harmony/baidu_map/src/main/ets/MapEnvConstant.ts
+++ harmony/baidu_map/src/main/ets/MapEnvConstant.ts
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import MapController from '@bdmap/map/src/main/ets/r/u/v/w';
+import MapController from '@bdmap/map/src/main/ets/d/g/h/i';
 
 export class MapEnvConstant {
   private static _instance: MapEnvConstant;
